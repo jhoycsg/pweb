b5d9545d10d7dc35a64ec7f4b9c1da5d9db3dc91
Siong Camelia <jhoy@eduspot-n2ng-070147.local.univ-savoie.fr>
2020-10-09 15:57:17 +0200

    a

--- function.php
+++ function.php
@@ -2,5 +2,9 @@
 function ajout_titre(){
   add_theme_support('title_tag');
 }
-add_action('after_setup_theme','ajout_titre');
+
+
+function ajout_syle(){
+  wp_enqueue_style('general',get_template_directory_uri();)
+}
 ?>
