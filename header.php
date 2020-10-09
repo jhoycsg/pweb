1e27c96d3dd3e568390dde2ec48342d1a03f4482
Siong Camelia <jhoy@eduspot-n2ng-070147.local.univ-savoie.fr>
2020-10-09 15:46:51 +0200

    modifs

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" type="text/css"> -->
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/theme.css" type="text/css" />
 	<?php wp_head()?>
 </head>
 
